65b8d69e7ba864c3afb2c1052d6bcc3eb413ba98
Mayara Dias <victdias.mayara@gmail.com>
2023-08-12 16:50:15 -0300

    Instanciando os componentes do adapter de publicacoes

--- app/src/main/java/com/dias/mayara/webook/adapter/PublicacoesAdapter.java
+++ app/src/main/java/com/dias/mayara/webook/adapter/PublicacoesAdapter.java
@@ -1,10 +1,17 @@
 package com.dias.mayara.webook.adapter;
 
+import android.media.Image;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.dias.mayara.webook.R;
+
+import org.w3c.dom.Text;
 
 public class PublicacoesAdapter extends RecyclerView.Adapter<PublicacoesAdapter.MyViewHolder> {
     @NonNull
@@ -25,10 +32,26 @@ public class PublicacoesAdapter extends RecyclerView.Adapter<PublicacoesAdapter.
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
+        private ImageView imageViewFotoPerfil;
+        private TextView textViewNomeAutorPost;
+        private TextView textViewNomeLivro;
+        private TextView textViewTextoPublicacao;
+        private TextView imageViewImagemPublicacao;
+        private TextView textViewNumeroCurtidas;
+        private TextView textViewNumeroComentarios;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageViewFotoPerfil = itemView.findViewById(R.id.imageViewFotoPerfil);
+            textViewNomeAutorPost = itemView.findViewById(R.id.textViewNomeAutorPost);
+            textViewNomeLivro = itemView.findViewById(R.id.textViewNomeLivro);
+            textViewTextoPublicacao = itemView.findViewById(R.id.textViewTextoPublicacao);
+            imageViewImagemPublicacao = itemView.findViewById(R.id.imageViewImagemPublicacao);
+            textViewNumeroCurtidas = itemView.findViewById(R.id.textViewNumeroCurtidas);
+            textViewNumeroComentarios = itemView.findViewById(R.id.textViewNumeroComentarios);
+
+
         }
     }
-
-
 }
