f3a532fc55ff6b139b26c2255cf568557af7f66e
Mayara Dias <victdias.mayara@gmail.com>
2023-09-08 17:02:43 -0300

    Inclusão do codigo que faz login do usuario no app

--- app/src/main/java/com/dias/mayara/webook/activity/LoginActivity.java
+++ app/src/main/java/com/dias/mayara/webook/activity/LoginActivity.java
@@ -1,16 +1,103 @@
 package com.dias.mayara.webook.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.dias.mayara.webook.R;
+import com.dias.mayara.webook.helper.ConfiguracaoFirebase;
+import com.dias.mayara.webook.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextLoginEmail;
+    private EditText editTextLoginSenha;
+    private Button buttonLogin;
+    private ProgressBar progressBar2;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inicializarComponentes();
+
+        // Fazer login do usuário
+        progressBar2.setVisibility(View.GONE);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoEmail = editTextLoginEmail.getText().toString();
+                String textoSenha = editTextLoginSenha.getText().toString();
+
+                // Se algum campo não tiver sido preenchido, envia uma mensagem de erro pro usuário
+                if(!textoEmail.isEmpty()) {
+                    if(!textoSenha.isEmpty()){
+
+                        usuario = new Usuario();
+                        usuario.setEmailUsuario(textoEmail);
+                        usuario.setSenhaUsuario(textoSenha);
+
+                        validarLogin(usuario);
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Preencha o campo Senha!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Preencha o campo Email!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void inicializarComponentes() {
+
+        editTextLoginEmail = findViewById(R.id.editTextLoginEmail);
+        editTextLoginSenha = findViewById(R.id.editTextLoginSenha);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        progressBar2 = findViewById(R.id.progressBar2);
+    }
+
+    public void validarLogin(Usuario usuario) {
+
+        progressBar2.setVisibility(View.VISIBLE);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmailUsuario(),
+                usuario.getSenhaUsuario()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+
+                    progressBar2.setVisibility(View.GONE);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                } else {
+
+                    Toast.makeText(LoginActivity.this, "Erro ao fazer login",
+                            Toast.LENGTH_SHORT).show();
+                    progressBar2.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
 }
