f779e14262d4074444f4204bf864b5e372a04b5b
Mayara Dias <victdias.mayara@gmail.com>
2023-09-11 23:12:16 -0300

    Implementação do recurso de fazer pesquisa de usuários (ainda não retorna os usuários na tela)

--- app/src/main/java/com/dias/mayara/webook/fragment/PesquisarFragment.java
+++ app/src/main/java/com/dias/mayara/webook/fragment/PesquisarFragment.java
@@ -2,14 +2,37 @@ package com.dias.mayara.webook.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.dias.mayara.webook.R;
+import com.dias.mayara.webook.helper.ConfiguracaoFirebase;
+import com.dias.mayara.webook.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PesquisarFragment extends Fragment {
+
+    private SearchView searchViewPesquisa;
+    private RecyclerView recyclerViewPesquisa;
+    private List<Usuario> listaUsuarios;
+    private DatabaseReference usuariosRef;
+    // TODO criar livrosRef
+    // TODO criar eventosRef
+    // Todo criar autoresRef
+
     public PesquisarFragment() {
         // Required empty public constructor
     }
@@ -17,6 +40,66 @@ public class PesquisarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pesquisar, container, false);
+        View view = inflater.inflate(R.layout.fragment_pesquisar, container, false);
+
+        searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
+        recyclerViewPesquisa = view.findViewById(R.id.recyclerViewPesquisa);
+
+        listaUsuarios = new ArrayList<>();
+        usuariosRef = ConfiguracaoFirebase.getFirebase().child("usuarios");
+
+        // Configuração da searchView
+        searchViewPesquisa.setQueryHint("Buscar");
+        searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String textoDigitado) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String textoDigitado) {
+
+                pesquisarUsuarios(textoDigitado);
+
+                return false;
+            }
+        });
+
+        return view;
+    }
+
+    private void pesquisarUsuarios(String textoDigitado) {
+
+        // Limpa a lista
+        listaUsuarios.clear();
+
+        // Confere se tem algum texto pra ser pesquisado
+        if(textoDigitado.length() > 0) {
+            Query query = usuariosRef.orderByChild("nome")
+                    .startAt(textoDigitado).endAt(textoDigitado + "\uf8ff");
+            // OrderByChild: Ordenar por nome que começa com textoDigitado ou termina com ele
+
+            // TODO adicionar nó no firebase para salvar os nomes com uppercase, pra fazer o filtro com ele
+
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot ds : snapshot.getChildren()){
+
+                        listaUsuarios.add(ds.getValue(Usuario.class));
+
+                    }
+
+                    int total = listaUsuarios.size();
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
     }
 }
